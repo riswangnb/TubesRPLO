9637e963c8b5fc54b8e2ae53b6b601cdefdfe8d7
riswangnb <riswangnb@gmail.com>
2025-12-26 14:21:42 +0800

    fix merge conflict

--- api/index.php
+++ api/index.php
@@ -1,18 +1,3 @@
 <?php
-echo "<h2>1. Isi Folder API (Tempat file ini berada):</h2>";
-$files_api = scandir(__DIR__);
-echo "<pre>" . print_r($files_api, true) . "</pre>";
 
-echo "<h2>2. Isi Folder Root (Satu level di atas):</h2>";
-$files_root = scandir(__DIR__ . '/../');
-echo "<pre>" . print_r($files_root, true) . "</pre>";
-
-echo "<h2>3. Cek Folder Public:</h2>";
-if (is_dir(__DIR__ . '/../public')) {
-    echo "Folder 'public' DITEMUKAN. Isinya:<br>";
-    $files_public = scandir(__DIR__ . '/../public');
-    echo "<pre>" . print_r($files_public, true) . "</pre>";
-} else {
-    echo "Folder 'public' TIDAK DITEMUKAN di root.";
-}
-?>
+require __DIR__ . '/../public/index.php';
